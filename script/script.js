9be8d82dd178187fcdb28e32da313c2308b3a8e3
Peter <goroh83@yahoo.com>
2017-12-11 23:04:25 -0500

    add carousel active dot script

--- script/script.js
+++ script/script.js
@@ -1,2 +1,15 @@
+let dots = document.querySelectorAll('.dot');
+var dotsIndex = 0;
+
+function printSlideIndex() {
+    i = this.currentSlide;
+    console.log(i);
+    let y = 0;
+    for ( y = 0; y < dots.length; y++) {
+        dots[y].classList.remove('dot-active');
+    }
+    dots[i].classList.add('dot-active');
+};
+
 document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
 document.querySelector('.next').addEventListener('click', () => mySiema.next());
